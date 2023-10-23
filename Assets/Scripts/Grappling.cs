5f11c6cba61a148799e21974bd0427d21c455f49
Liang-Tong <kiwilt@icloud.com>
2023-10-23 11:04:22 +0200

    first commit

---
+++ Assets/Scripts/Grappling.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grappling : MonoBehaviour
+{
+    [Header("References")]
+    private PlayerMovement pm;
+    [SerializeField] public Transform cam;
+    [SerializeField] public Transform gunTip;
+    [SerializeField] public LayerMask whatIsGrappleable;
+    [SerializeField] public LineRenderer lr;
+
+    [Header("Grappling")]
+    [SerializeField] public float maxGrappleDistance;
+    [SerializeField] public float grappleDelayTime;
+    [SerializeField] public float overshootYAxis;
+
+    private Vector3 grapplePoint;
+
+    [Header("Cooldown")]
+    [SerializeField] public float grapplingCd;
+    [SerializeField] public float grapplingCdTimer;
+
+    [Header("Input")]
+    [SerializeField] public KeyCode grappleKey = KeyCode.Mouse1;
+
+    private bool grappling;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pm = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(grappleKey)) startGrapple();
+
+        if (grapplingCdTimer > 0)
+        {
+            grapplingCdTimer -= Time.deltaTime;
+        }
+        if (Input.GetKeyUp(grappleKey)) stopGrapple();
+    }
+
+    private void LateUpdate()
+    {
+        if (grappling)
+        {
+            lr.SetPosition(0, gunTip.position);
+        }
+    }
+
+    private void startGrapple()
+    {
+        if (grapplingCdTimer > 0) return;
+
+        grappling = true;
+
+        pm.freeze = true;
+
+        RaycastHit hit;
+        if (Physics.Raycast(cam.position, cam.forward, out hit, maxGrappleDistance, whatIsGrappleable))
+        {
+            grapplePoint = hit.point;
+
+            Invoke(nameof(executeGrapple), grappleDelayTime);
+        }
+        else
+        {
+            grapplePoint = cam.position + cam.forward * maxGrappleDistance;
+
+            Invoke(nameof(stopGrapple), grappleDelayTime);
+        }
+        lr.enabled = true;
+        lr.SetPosition(1, grapplePoint);
+    }
+
+    private void executeGrapple()
+    {
+        pm.freeze = false;
+
+        Vector3 lowestPoint = new Vector3(transform.position.x, transform.position.y - 1f, transform.position.z);
+
+        float grapplePointRelativeYPos = grapplePoint.y - lowestPoint.y;
+        float highestPointOnArc = grapplePointRelativeYPos + overshootYAxis;
+
+        if (grapplePointRelativeYPos < 0) highestPointOnArc = overshootYAxis;
+
+        pm.jumpToPosition(grapplePoint, highestPointOnArc);
+
+        //Invoke(nameof(stopGrapple), 1f);
+    }
+
+    private void stopGrapple()
+    {
+        pm.freeze = false;
+        pm.activeGrapple = false;
+        grappling = false;
+
+        grapplingCdTimer = grapplingCd;
+
+        lr.enabled = false;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    private bool jumping;
+    private float jumpTime;
+    private float buttonTime = 0.3f;
+    public bool freeze;
+    public bool activeGrapple;
+    public float groundDrag;
+
+    public LayerMask whatisGround;
+
+    [Header("Slopehandle")]
+    public float maxSlopeAngle;
+
+    private Vector3 moveDirection;
+    private Transform orientation;
+    private RaycastHit slopeHit;
+    private bool grounded;
+    private bool doubleJump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        orientation = cam.transform;
+        moveDirection = orientation.forward * Input.GetAxis("Vertical") + orientation.right * Input.GetAxis("Horizontal");
+        if (activeGrapple) return;
+        //if (freeze)
+        //{
+        //    rb.velocity = Vector3.zero;
+        //}
+        if (grounded)
+        {
+            Vector3 newVelocity = cam.transform.forward * Input.GetAxis("Vertical") * speed;
+            newVelocity += cam.transform.right * Input.GetAxis("Horizontal") * speed;
+            newVelocity.y = rb.velocity.y;
+            rb.velocity = newVelocity;
+        }
+    }
+
+    private void Update()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, 0.5f + 0.1f, whatisGround);
+        if (grounded)
+        {
+            doubleJump = true;
+        }
+        OnSlope();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (grounded)
+            {
+                jumping = true;
+                jumpTime = 0;
+            } else if (doubleJump)
+            {
+                doubleJump = false;
+                jumping = true;
+                jumpTime = 0;
+            }
+
+        }
+        if (jumping)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            jumpTime += Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.Space) | jumpTime > buttonTime)
+        {
+            jumping = false;
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit hit;
+            Ray downRay = new Ray(transform.position, Vector3.down);
+            Physics.Raycast(downRay, out hit);
+            Debug.Log(hit.distance);
+        }
+    }
+
+
+    public void jumpToPosition(Vector3 targetPosition, float trajectoryHeight)
+    {
+        activeGrapple = true;
+        velocityToSet = CalculateJumpVelocity(transform.position, targetPosition, trajectoryHeight);
+        Invoke(nameof(SetVelocity), 0.1f);
+    }
+    private Vector3 velocityToSet;
+    private void SetVelocity()
+    {
+        rb.velocity = velocityToSet;
+    }
+
+    private Vector3 GetSlopeMoveDirection()
+    {
+        return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
+    }
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, cam.transform.position.y * 0.5f + 0.3f))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+
+        return false;
+    }
+    public Vector3 CalculateJumpVelocity(Vector3 startPoint, Vector3 endPoint, float trajectoryHeight)
+    {
+        float gravity = Physics.gravity.y;
+        float displacementY = endPoint.y - startPoint.y;
+        Vector3 displacementXZ = new Vector3(endPoint.x - startPoint.x, 0f, endPoint.z - startPoint.z);
+
+        Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * trajectoryHeight);
+        Vector3 velocityXZ = displacementXZ / (Mathf.Sqrt(-2 * trajectoryHeight / gravity)
+            + Mathf.Sqrt(2 * (displacementY - trajectoryHeight) / gravity));
+
+        return velocityXZ + velocityY;
+    }
+}
